088503cdc2b039e44bbc7c7e5efa846dc6d8a317
Stuart Berg <bergs@janelia.hhmi.org>
2012-05-15 11:14:33 -0400

    Reworked Op5ifyer so it reorders axes in addition to inserting missing axes.

--- volumina/adaptors.py
+++ volumina/adaptors.py
@@ -34,25 +34,46 @@ if _has_lazyflow:
                 defaultTags = vigra.defaultAxistags('txyzc')
                 
                 for tag in [tag for tag in defaultTags if tag not in inputAxistags]:
-                    inputAxistags.insert(defaultTags.index(tag.key),tag)
-                    inputShape.insert(defaultTags.index(tag.key),1)
+                    #inputAxistags.insert(defaultTags.index(tag.key),tag)
+                    #inputShape.insert(defaultTags.index(tag.key),1)
                     self.resSl.insert(defaultTags.index(tag.key),0)
                 
+                outputShape = []
+                for tag in defaultTags:
+                    if tag in inputAxistags:
+                        outputShape += [ inputShape[ inputAxistags.index(tag.key) ] ]
+                    else:
+                        outputShape += [1]                
+                
                 self.outputs["output"]._dtype = self.inputs["input"]._dtype
-                self.outputs["output"]._shape = tuple(inputShape)
-                self.outputs["output"]._axistags = inputAxistags
+                self.outputs["output"]._shape = tuple(outputShape)
+                self.outputs["output"]._axistags = defaultTags
                 
             def execute(self,slot,roi,result):
                 
                 sl = [slice(0,roi.stop[i]-roi.start[i],None) if sl != 0\
-                      else 0 for i,sl in enumerate(self.resSl)]
+                      else slice(0,1) for i,sl in enumerate(self.resSl)]
                 
-                roi.start = TinyVector([i for i,k in zip(roi.start,sl) if k != 0])
-                roi.stop = TinyVector([i for i,k in zip(roi.stop,sl) if k != 0])
+                inputTags = self.input.meta.axistags
                 
-                tmpres = self.inputs["input"](start=roi.start,stop=roi.stop).wait()
-                result[sl] = tmpres
-                return result
+                # Convert the requested slice into a slice for our input
+                outSlice = roi.toSlice()
+                inSlice = [None] * len(inputTags)
+                for i, s in enumerate(outSlice):
+                    tagKey = self.output.meta.axistags[i].key
+                    inputAxisIndex = inputTags.index(tagKey)
+                    if inputAxisIndex < len(inputTags):
+                        inSlice[inputAxisIndex] = s
+                
+#                roi.start = TinyVector([i for i,k in zip(roi.start,sl) if k != 0])
+#                roi.stop = TinyVector([i for i,k in zip(roi.stop,sl) if k != 0])
+                
+                tmpres = self.inputs["input"][inSlice].wait()
+                
+                # Re-order the axis the way volumina expects them
+                v = tmpres.view(vigra.VigraArray)
+                v.axistags = inputTags
+                result[sl] = v.withAxes(*list('txyzc'))
 
 class Array5d( object ):
     '''Embed a array with dim = 3 into the volumina coordinate system.'''
@@ -73,4 +94,5 @@ class Array5d( object ):
     def astype( self, dtype):
         return Array5d( self.a, dtype )
 
-
+if __name__ == "__main__":
+    pass
