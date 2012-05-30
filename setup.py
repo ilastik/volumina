31f1fefb00d0a0c052d802c5a1e1d2775d324b79
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-05-30 16:56:07 +0200

    fixed setup.py -> really works now

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 packages=['volumina', 
           'volumina.pixelpipeline',
           'volumina.colorama',
-          'volumina.layerwidget',
+          'volumina.widgets',
           'volumina.view3d',
           'volumina.resources',
           'volumina.resources.icons']
@@ -12,7 +12,7 @@ package_data={'volumina.resources.icons': ['*.png', 'LICENSES']}
 
 setup(name='volumina',
       version='0.6a',
-      description='Volume Slicer and Editor',
+      description='Volume Slicing and Editing',
       url='https://github.com/Ilastik/volumina',
       packages=packages,
       package_data=package_data
