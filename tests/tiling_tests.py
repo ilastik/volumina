d5f4d6a7e1c98d94bffe28918f10c1a05c66dae8
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-07-17 09:59:17 +0200

    some TileProvider testing

--- tests/tiling_tests.py
+++ tests/tiling_tests.py
@@ -13,20 +13,27 @@ from volumina.pixelpipeline.imagepump import StackedImageSources
 
 class TileProviderTest( ut.TestCase ):
     def setUp( self ):
-        self.ds = ConstantSource()
+        self.GRAY1 = 60
+        self.ds1 = ConstantSource( self.GRAY1 )
 
-        self.layer1 = GrayscaleLayer( self.ds )
+        self.GRAY2 = 120
+        self.ds2 = ConstantSource( self.GRAY2 )
+
+        self.GRAY3 = 190
+        self.ds3 = ConstantSource( self.GRAY3 )
+
+        self.layer1 = GrayscaleLayer( self.ds1 )
         self.layer1.visible = False
         self.layer1.opacity = 0.1
-        self.ims1 = GrayscaleImageSource( self.ds, self.layer1 )
-        self.layer2 = GrayscaleLayer( self.ds )
+        self.ims1 = GrayscaleImageSource( self.ds1, self.layer1 )
+        self.layer2 = GrayscaleLayer( self.ds2 )
         self.layer2.visible = True
         self.layer2.opacity = 0.3
-        self.ims2 = GrayscaleImageSource( self.ds, self.layer2 )
-        self.layer3 = GrayscaleLayer( self.ds ) 
+        self.ims2 = GrayscaleImageSource( self.ds2, self.layer2 )
+        self.layer3 = GrayscaleLayer( self.ds3 ) 
         self.layer3.visible = True
         self.layer3.opacity = 1.0
-        self.ims3 = GrayscaleImageSource( self.ds, self.layer3 )
+        self.ims3 = GrayscaleImageSource( self.ds3, self.layer3 )
 
     def test( self ):
         lsm = LayerStackModel()
@@ -41,7 +48,7 @@ class TileProviderTest( ut.TestCase ):
 
         try:
             tp = TileProvider(tiling, sims)
-            tp.getTiles(QRectF(100,100,200,300))
+            tiles = tp.getTiles(QRectF(100,100,200,300))
         finally:
             tp.notifyThreadsToStop()
         
