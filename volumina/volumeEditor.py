170ec2ee604bbc4e9436d4beea3a9a6eec314b2d
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2012-07-23 17:18:02 +0200

    missing import

--- volumina/volumeEditor.py
+++ volumina/volumeEditor.py
@@ -21,6 +21,7 @@ useVTK = True
 try:
     from view3d.view3d import OverviewScene
 except ImportError:
+    import warnings
     warnings.warn("could not import optional dependency VTK")
     useVTK = False
 
