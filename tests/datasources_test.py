c47e4037eae015316e3b1f98861610890e406437
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2012-07-23 16:09:15 +0200

    Move tests contained in datasources.py to test/ dir
    fixes #109

---
+++ tests/datasources_test.py
@@ -0,0 +1,124 @@
+import unittest as ut
+from abc import ABCMeta, abstractmethod
+from volumina.pixelpipeline.datasources import ArraySource, RelabelingArraySource
+import numpy as np
+
+try:
+    import lazyflow
+    has_lazyflow = True
+except ImportError:
+    has_lazyflow = False
+
+if has_lazyflow:
+    from lazyflow.graph import Graph
+    from volumina.pixelpipeline._testing import OpDataProvider
+    from volumina.pixelpipeline.datasources import LazyflowSource
+
+class GenericArraySourceTest:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def setUp( self ):
+        self.source = None
+
+    def testRequestWait( self ):
+        slicing = (slice(0,1),slice(10,20), slice(20,25), slice(0,1), slice(0,1))
+        requested = self.source.request(slicing).wait()
+        self.assertTrue(np.all(requested == self.raw[0:1,10:20,20:25,0:1,0:1]))
+
+    def testRequestNotify( self ):
+        slicing = (slice(0,1),slice(10,20), slice(20,25), slice(0,1), slice(0,1))
+        request = self.source.request(slicing)
+        
+        def check(result, codon):
+            self.assertTrue(np.all(result == self.raw[0:1,10:20,20:25,0:1,0:1]))
+            self.assertEqual(codon, "unique")
+        request.notify(check, codon="unique")
+
+    def testSetDirty( self ):
+        self.signal_emitted = False
+        self.slicing = (slice(0,1),slice(10,20), slice(20,25), slice(0,1), slice(0,1))
+
+        def slot( sl ):
+            self.signal_emitted = True
+            self.assertTrue( sl == self.slicing )
+
+        self.source.isDirty.connect(slot)
+        self.source.setDirty( self.slicing )
+        self.source.isDirty.disconnect(slot)
+
+        self.assertTrue( self.signal_emitted )
+
+        del self.signal_emitted
+        del self.slicing
+
+
+class ArraySourceTest( ut.TestCase, GenericArraySourceTest ):
+    def setUp( self ):
+        import numpy as np
+        from scipy.misc import lena
+        self.lena = lena()
+
+        self.raw = np.zeros((1,512,512,1,1))
+        self.raw[0,:,:,0,0] = self.lena
+        self.source = ArraySource( self.raw )
+
+class RelabelingArraySourceTest( ut.TestCase, GenericArraySourceTest ):
+    def setUp( self ):
+        a = np.zeros((5,1,1,1,1), dtype=np.uint32)
+        #the data contained in a ranges from [1,5]
+        a[:,0,0,0,0] = np.arange(0,5)
+        self.source = RelabelingArraySource(a)
+
+        #we apply the relabeling i -> i+1
+        relabeling = np.arange(1,a.max()+2, dtype=np.uint32)
+        self.source.setRelabeling(relabeling)
+
+    def testRequestWait( self ):
+        slicing = (slice(0,5),slice(None), slice(None), slice(None), slice(None))
+        requested = self.source.request(slicing).wait()
+        assert requested.ndim == 5
+        self.assertTrue(np.all(requested.flatten() == np.arange(1,6, dtype=np.uint32)))
+
+    def testRequestNotify( self ):
+        slicing = (slice(0,5),slice(None), slice(None), slice(None), slice(None))
+        request = self.source.request(slicing)
+        
+        def check(result, codon):
+            self.assertTrue(np.all(result.flatten() == np.arange(1,6, dtype=np.uint32)))
+            self.assertEqual(codon, "unique")
+        request.notify(check, codon="unique")
+
+    def testSetDirty( self ):
+        self.signal_emitted = False
+        self.slicing = (slice(0,5),slice(None), slice(None), slice(None), slice(None))
+
+        def slot( sl ):
+            self.signal_emitted = True
+            self.assertTrue( sl == self.slicing )
+
+        self.source.isDirty.connect(slot)
+        self.source.setDirty( self.slicing )
+        self.source.isDirty.disconnect(slot)
+
+        self.assertTrue( self.signal_emitted )
+
+        del self.signal_emitted
+        del self.slicing
+
+
+    class LazyflowSourceTest( ut.TestCase, GenericArraySourceTest ):
+        def setUp( self ):
+            import numpy as np
+            self.np = np
+            from scipy.misc import lena
+            self.lena = lena()
+            self.raw = np.zeros((1,512,512,1,1), dtype=np.uint8)
+            self.raw[0,:,:,0,0] = self.lena
+
+            g = Graph()
+            op = OpDataProvider(g, self.raw)
+            self.source = LazyflowSource(op.Data, "Data")
+
+if __name__ == '__main__':
+    ut.main()
