8647372a6fea502f16d286b6f50bfc174b09aec9
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-07-26 18:50:49 +0200

    layerstack test: testing moving layers up and down

--- tests/layerstack_test.py
+++ tests/layerstack_test.py
@@ -3,40 +3,95 @@ from volumina.layer import Layer
 from volumina.layerstack import LayerStackModel
 
 class LayerStackModelTest( ut.TestCase ):
-    def testAddingAndRemoving( self ):
-        l1 = Layer()
-        l1.name = 'l1'
-        l2 = Layer()
-        l2.name = 'l2'
-        l3 = Layer()
-        l3.name = 'l3'
+    def setUp( self ):
+        self.l1 = Layer()
+        self.l1.name = 'l1'
+        self.l2 = Layer()
+        self.l2.name = 'l2'
+        self.l3 = Layer()
+        self.l3.name = 'l3'
 
+    def testAddingAndRemoving( self ):
         lsm = LayerStackModel()
         self.assertEqual(len(lsm), 0)
 
-        lsm.append(l1)
+        lsm.append(self.l1)
         self.assertEqual(len(lsm), 1)
-        self.assertEqual(lsm[0].name, l1.name )
+        self.assertEqual(lsm[0].name, self.l1.name )
 
-        lsm.append(l2)
+        lsm.append(self.l2)
         self.assertEqual(len(lsm), 2)
-        self.assertEqual(lsm[0].name, l2.name )
-        self.assertEqual(lsm[1].name, l1.name )
+        self.assertEqual(lsm[0].name, self.l2.name )
+        self.assertEqual(lsm[1].name, self.l1.name )
 
-        lsm.insert(1, l3)
+        lsm.insert(1, self.l3)
         self.assertEqual(len(lsm), 3)
-        self.assertEqual(lsm[0].name, l2.name )
-        self.assertEqual(lsm[1].name, l3.name )
-        self.assertEqual(lsm[2].name, l1.name )
+        self.assertEqual(lsm[0].name, self.l2.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l1.name )
 
         lsm.selectRow( 0 )
         lsm.deleteSelected()
         self.assertEqual(len(lsm), 2)
-        self.assertEqual(lsm[0].name, l3.name )
-        self.assertEqual(lsm[1].name, l1.name )
+        self.assertEqual(lsm[0].name, self.l3.name )
+        self.assertEqual(lsm[1].name, self.l1.name )
 
         lsm.clear()
         self.assertEqual(len(lsm), 0)
+
+    def testMovingLayers( self ):
+        lsm = LayerStackModel()
+        lsm.append(self.l1)
+        lsm.append(self.l2)
+        lsm.append(self.l3)
+        lsm.selectRow(1)
+        self.assertEqual(len(lsm),3)
+        self.assertEqual(lsm[0].name, self.l3.name )
+        self.assertEqual(lsm[1].name, self.l2.name )
+        self.assertEqual(lsm[2].name, self.l1.name )
+        self.assertEqual(lsm.selectedRow(), 1)
+
+        lsm.moveSelectedDown()
+        self.assertEqual(lsm.selectedRow(), 2)
+        self.assertEqual(len(lsm),3)
+        self.assertEqual(lsm[0].name, self.l3.name )
+        self.assertEqual(lsm[1].name, self.l1.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+
+        lsm.selectRow(1)
+        lsm.moveSelectedUp()
+        self.assertEqual(lsm.selectedRow(), 0)
+        self.assertEqual(len(lsm),3)
+        self.assertEqual(lsm[0].name, self.l1.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+
+
+        # moving topmost layer up => nothing should happen
+        lsm.selectRow(0)
+        self.assertEqual(lsm.selectedRow(), 0)
+        self.assertEqual(lsm[0].name, self.l1.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+        lsm.moveSelectedUp()
+        self.assertEqual(lsm.selectedRow(), 0)
+        self.assertEqual(lsm[0].name, self.l1.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+
+        # moving bottommost layer down => nothing should happen
+        lsm.selectRow(2)
+        self.assertEqual(lsm.selectedRow(), 2)
+        self.assertEqual(lsm[0].name, self.l1.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+        lsm.moveSelectedDown()
+        self.assertEqual(lsm.selectedRow(), 2)
+        self.assertEqual(lsm[0].name, self.l1.name )
+        self.assertEqual(lsm[1].name, self.l3.name )
+        self.assertEqual(lsm[2].name, self.l2.name )
+
+
 
 if __name__=='__main__':
     ut.main()
