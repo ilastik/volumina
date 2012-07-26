2e5b436b02ad219a68520f7a5c8780962ad11f72
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-07-26 12:10:42 +0200

    added volumina._testing to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,15 @@ packages=['volumina',
           'volumina.widgets.ui',
           'volumina.view3d',
           'volumina.resources',
-          'volumina.resources.icons']
+          'volumina.resources.icons',
+          'volumina._testing']
 
 package_data={'volumina.resources.icons': ['*.png', 'LICENSES'],
-              'volumina.widgets.ui': ['*.ui']}
+              'volumina.widgets.ui': ['*.ui'],
+              'volumina._testing': ['*.tif',
+                                    '*.png', 
+                                    'lena.npy', 
+                                    'rgba129x104.npy']}
 
 setup(name='volumina',
       version='0.6a',
