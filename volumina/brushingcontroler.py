ilastik/volumina

commit e68ec63f38059082c41330e0ae60d907deb834ce

Navigation: Panning can be performed with shift+click (in addition to middle-click).

volumina/brushingcontroler.py
@@ -74,7 +74,7 @@ class BrushingInterpreter( QObject ):
         ### the following implements a simple state machine
         if self._current_state == self.DEFAULT_MODE:
             ### default mode -> draw mode
-            if etype == QEvent.MouseButtonPress and event.button() == Qt.LeftButton:
+            if etype == QEvent.MouseButtonPress and event.button() == Qt.LeftButton and event.modifiers == Qt.NoModifier:
                 # navigation interpreter also has to be in 
                 # default mode to avoid inconsistencies
                 if self._navIntr.state == self._navIntr.DEFAULT_MODE:

volumina/navigationControler.py
@@ -53,7 +53,8 @@ class NavigationInterpreter(QObject):
         ### the following implements a simple state machine
         if self._current_state == self.DEFAULT_MODE:
             ### default mode -> drag mode
-            if etype == QEvent.MouseButtonPress and event.button() == Qt.MidButton:
+            if    (etype == QEvent.MouseButtonPress and event.button() == Qt.MidButton) \
+               or (etype == QEvent.MouseButtonPress and event.modifiers() == Qt.ShiftModifier):
                 # self.onExit_default(): call it here, if needed
                 self._current_state = self.DRAG_MODE
                 self.onEntry_drag( watched, event )
@@ -74,7 +75,7 @@ class NavigationInterpreter(QObject):
                 return True
         elif self._current_state == self.DRAG_MODE:
             ### drag mode -> default mode
-            if etype == QEvent.MouseButtonRelease and event.button() == Qt.MidButton:
+            if etype == QEvent.MouseButtonRelease:
                 self.onExit_drag( watched, event)
                 self._current_state = self.DEFAULT_MODE
                 self.onEntry_default( watched, event )

commit ab491ebaac8017ac1e29ef394ce4a605e6390cd2

fix drawing no working, bug introduced in e68ec63f38059082c41330e0ae60d907deb834ce

volumina/brushingcontroler.py
@@ -74,7 +74,7 @@ class BrushingInterpreter( QObject ):
         ### the following implements a simple state machine
         if self._current_state == self.DEFAULT_MODE:
             ### default mode -> draw mode
-            if etype == QEvent.MouseButtonPress and event.button() == Qt.LeftButton and event.modifiers == Qt.NoModifier:
+            if etype == QEvent.MouseButtonPress and event.button() == Qt.LeftButton and event.modifiers() == Qt.NoModifier:
                 # navigation interpreter also has to be in 
                 # default mode to avoid inconsistencies
                 if self._navIntr.state == self._navIntr.DEFAULT_MODE: