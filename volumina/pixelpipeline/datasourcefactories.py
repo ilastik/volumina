ilastik/volumina

commit a3fb2bb7d32a71023366fab9c797a196314d1686

RelabelingArraySource: allow to a single relabeling entry relableling[index] to a specific value Viewer: new function to add a random color layer (for segmentations), color layer Datasources: fix createDatasource function, which would make (unnecessary) copies of the data with the wrong dtype

volumina/pixelpipeline/datasourcefactories.py
@@ -31,21 +31,13 @@ def createDataSource(source,withShape = False):
     #has to handle NumpyArray
     #check if the array is 5d, if not so embed it in a canonical way
     if len(source.shape) == 2:
-        tmp = numpy.ndarray((1,source.shape[0],source.shape[1],1,1))
-        tmp[0,:,:,0,0] = source
-        source = tmp
+        source.shape = (1,) + source.shape + (1,1)
     elif len(source.shape) == 3 and source.shape[2] <= 4:
-        tmp = numpy.ndarray((1,source.shape[0],source.shape[1],source.shape[2],1))
-        tmp[0,:,:,0,:] = source
-        source = tmp
+        source.shape = (1,) + source.shape[0:2] + (1,) + source.shape[2]
     elif len(source.shape) == 3:
-        tmp = numpy.ndarray((1,source.shape[0],source.shape[1],source.shape[2],1))
-        tmp[0,:,:,:,0] = source
-        source = tmp
+        source.shape = (1,) + source.shape + (1,)
     elif len(source.shape) == 4:
-        tmp = numpy.ndarray((1,source.shape[0],source.shape[1],source.shape[2],source.shape[3]))
-        tmp[0,:,:,:,:] = source
-        source = tmp
+        source.shape = (1,) + source.shape
     src = ArraySource(source)
     if withShape:
         return src,source.shape

volumina/api.py
@@ -8,3 +8,11 @@ from volumina.volumeEditorWidget import VolumeEditorWidget
 from volumina.widgets.layerwidget import LayerWidget
 
 from volumina.viewer import Viewer
+
+from PyQt4.QtGui import QApplication
+import sys
+
+def viewerApp():
+    app = QApplication(sys.argv)
+    v = Viewer()
+    return (v, app)

volumina/pixelpipeline/datasources.py
@@ -113,9 +113,21 @@ class RelabelingArraySource( ArraySource ):
         self._relabeling = None
         
     def setRelabeling( self, relabeling ):
+        """Sets new relabeling vector. It should have a len(relabling) == max(your data)+1
+           and give, for each possible data value x, the relabling as relabeling[x]."""   
         assert relabeling.dtype == self._array.dtype
         self._relabeling = relabeling
         self.setDirty(5*(slice(None),))
+
+    def setRelabelingEntry( self, index, value, setDirty=True ):
+        """Sets the entry for data value index to value, such that afterwards
+           relabeling[index] =  value.
+           
+           If setDirty is true, the source will signal dirtyness. If you plan to issue many calls to this function
+           in a loop, setDirty to true only on the last call."""
+        self._relabeling[index] = value
+        if setDirty:
+            self.setDirty(5*(slice(None),))
 
     def request( self, slicing ):
         if not is_pure_slicing(slicing):

volumina/viewer.py
@@ -107,7 +107,7 @@ class Viewer(QMainWindow):
                 viewAxis = [i for i in range(1,4) if self.dataShape[i] != 1][0] - 1
                 self.viewer.quadview.switchMinMax(viewAxis)    
         
-    def addGrayscaleLayer(self,a, name=None):
+    def addGrayscaleLayer(self, a, name=None):
         source,self.dataShape = createDataSource(a,True)
         layer = GrayscaleLayer(source)
         if name:
@@ -115,7 +115,7 @@ class Viewer(QMainWindow):
         self.layerstack.append(layer)
         return layer
         
-    def addAlphaModulatedLayer(self,a, name=None):
+    def addAlphaModulatedLayer(self, a, name=None):
         source,self.dataShape = createDataSource(a,True)
         layer = AlphaModulatedLayer(source)
         if name:
@@ -123,7 +123,7 @@ class Viewer(QMainWindow):
         self.layerstack.append(layer)
         return layer
     
-    def addRGBALayer(self,a, name=None):
+    def addRGBALayer(self, a, name=None):
         source,self.dataShape = createDataSource(a,True)
         layer = RGBALayer(source[0],source[1],source[2])
         if name:
@@ -131,6 +131,32 @@ class Viewer(QMainWindow):
         self.layerstack.append(layer)
         return layer
 
+    def addRandomColorsLayer(self, a, name=None):
+        return self.addColorTableLayer(a, name, colortable=None)
+    
+    def addColorTableLayer(self, a, name=None, colortable=None):
+        if colortable is None:
+            colortable = self._randomColors()
+        source,self.dataShape = createDataSource(a,True)
+        layer = ColortableLayer(source, colortable)
+        if name:
+            layer.name = name
+        self.layerstack.append(layer)
+        return layer
+
+    def _randomColors(self, M=256):
+        """Generates a pleasing color table with M entries."""
+
+        colors = []
+        for i in range(M):
+            if i == 0:
+                colors.append(QColor(0, 0, 0, 0).rgba())
+            else:
+                h, s, v = random.random(), random.random(), 1.0
+                color = numpy.asarray(colorsys.hsv_to_rgb(h, s, v)) * 255
+                qColor = QColor(*color)
+                colors.append(qColor.rgba())
+        return colors
         
 if __name__ == "__main__":
     

commit 88e513e9140c63d43d8ccaf07b92b448986e06a2

datasourcefactories: ArraySources should work with a view of the original array without modifying the original array's shape. Fixes the unit test failure introduced in a3fb2bb7d32a71023366fab9c797a196314d1686.

volumina/pixelpipeline/datasourcefactories.py
@@ -31,13 +31,13 @@ def createDataSource(source,withShape = False):
     #has to handle NumpyArray
     #check if the array is 5d, if not so embed it in a canonical way
     if len(source.shape) == 2:
-        source.shape = (1,) + source.shape + (1,1)
+        source.reshape( (1,) + source.shape + (1,1) )
     elif len(source.shape) == 3 and source.shape[2] <= 4:
-        source.shape = (1,) + source.shape[0:2] + (1,) + source.shape[2]
+        source.reshape( (1,) + source.shape[0:2] + (1,) + source.shape[2] )
     elif len(source.shape) == 3:
-        source.shape = (1,) + source.shape + (1,)
+        source.reshape( (1,) + source.shape + (1,) )
     elif len(source.shape) == 4:
-        source.shape = (1,) + source.shape
+        source.reshape( (1,) + source.shape )
     src = ArraySource(source)
     if withShape:
         return src,source.shape