b2b32c175d6bdf1e520404ec1674a6f668a9e4e8
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-01-24 09:08:51 +0100

    comment out broken 2D viewing code so that 2D images can at least be displayed

--- volumina/viewer.py
+++ volumina/viewer.py
@@ -108,10 +108,11 @@ class Viewer(QMainWindow):
             #(some signal/slot connections may be set up in the above init)
             self.editor.dataShape = s
 
+            #FIXME: this code is broken
             #if its 2D, maximize the corresponding window
-            if len([i for i in list(self.dataShape)[1:4] if i == 1]) == 1:
-                viewAxis = [i for i in range(1,4) if self.dataShape[i] == 1][0] - 1
-                self.viewer.quadview.switchMinMax(viewAxis)    
+            #if len([i for i in list(self.dataShape)[1:4] if i == 1]) == 1:
+            #    viewAxis = [i for i in range(1,4) if self.dataShape[i] == 1][0] - 1
+            #    self.viewer.quadview.switchMinMax(viewAxis)    
         
     def addGrayscaleLayer(self, a, name=None, direct=False):
         source,self.dataShape = createDataSource(a,True)
