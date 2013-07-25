590d476bb16059dd5d4bb3ec83f9cfdc58104526
Stuart Berg <bergs@janelia.hhmi.org>
2013-07-25 14:13:10 -0400

    DataExportOptionsDlg: Cleanups.

--- volumina/widgets/dataExportOptionsDlg.py
+++ volumina/widgets/dataExportOptionsDlg.py
@@ -1,16 +1,18 @@
 import os
 import collections
 from functools import partial
-from abc import abstractmethod
+
+import numpy
 
 from PyQt4 import uic
-from PyQt4.QtCore import Qt, QEvent
-from PyQt4.QtGui import QDialog, QValidator
-
-from multiformatSlotExportFileOptionsWidget import MultiformatSlotExportFileOptionsWidget
+from PyQt4.QtCore import Qt, QObject, QEvent
+from PyQt4.QtGui import QDialog, QValidator, QDialogButtonBox
 
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 
+#**************************************************************************
+# Model operator interface ABC
+#**************************************************************************
 class ExportOperatorABC(Operator):
     """
     The export dialog is designed to work with any operator that satisfies this ABC interface.
@@ -42,16 +44,10 @@ class ExportOperatorABC(Operator):
     ImageToExport = OutputSlot() # Preview of the pre-processed image that will be exported
     ExportPath = OutputSlot() # Location of the saved file after export is complete.
 
-    @abstractmethod
-    def run_export(self):
-        raise NotImplementedError
-
     @classmethod
     def __subclasshook__(cls, C):
         # Must have all the required input and output slots.
         if cls is ExportOperatorABC:
-#            if not hasattr(C, 'run_export'):
-#                return False
             for slot in cls.inputSlots:
                 if not hasattr(C, slot.name) or not isinstance(getattr(C, slot.name), InputSlot):
                     return False
@@ -61,6 +57,9 @@ class ExportOperatorABC(Operator):
             return True
         return NotImplemented
 
+#**************************************************************************
+# DataExportOptionsDlg
+#**************************************************************************
 class DataExportOptionsDlg(QDialog):
     
     def __init__(self, parent, opExportSlot):
@@ -73,14 +72,14 @@ class DataExportOptionsDlg(QDialog):
                              If the user clicks 'OK', then copy the slot settings from the temporary op to your real one.
         """
         super( DataExportOptionsDlg, self ).__init__(parent)
+        uic.loadUi( os.path.splitext(__file__)[0] + '.ui', self )
+
+        self._opExportSlot = opExportSlot
         assert isinstance( opExportSlot, ExportOperatorABC ), \
             "Cannot use {} as an export operator.  "\
             "It doesn't match the required interface".format( type(opExportSlot) )
-        
-        uic.loadUi( os.path.splitext(__file__)[0] + '.ui', self )
-        self._opExportSlot = opExportSlot
-        self.installEventFilter(self)
 
+        # Init child widgets
         self._initMetaInfoWidgets()
         self._initSubregionWidget()
         self._initDtypeConversionWidgets()
@@ -88,12 +87,30 @@ class DataExportOptionsDlg(QDialog):
         self._initAxisOrderWidgets()
         self._initFileOptionsWidget()
 
+        # See self.eventFilter()
+        self.installEventFilter(self)
+
+    def eventFilter(self, watched, event):
+        # Ignore 'enter' keypress events, since the user may just be entering settings.
+        # The user must manually click the 'OK' button to close the dialog.
+        if watched == self and \
+           event.type() == QEvent.KeyPress and \
+           ( event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return):
+            return True
+        return False
+
+    #**************************************************************************
+    # Input/Output Meta-info (display only)
+    #**************************************************************************
     def _initMetaInfoWidgets(self):
         ## Input/output meta-info display widgets
         opExportSlot = self._opExportSlot
         self.inputMetaInfoWidget.initSlot( opExportSlot.Input )
         self.outputMetaInfoWidget.initSlot( opExportSlot.ImageToExport )
 
+    #**************************************************************************
+    # Subregion roi
+    #**************************************************************************
     def _initSubregionWidget(self):
         opExportSlot = self._opExportSlot
         inputAxes = opExportSlot.Input.meta.getAxisKeys()
@@ -121,8 +138,22 @@ class DataExportOptionsDlg(QDialog):
 
         self.roiWidget.roiChanged.connect( _handleRoiChange )
 
+    #**************************************************************************
+    # Dtype conversion
+    #**************************************************************************
     def _initDtypeConversionWidgets(self):
-        self.convertDtypeCheckbox.toggled.connect( self._handleConvertDtypeChecked )
+        def _selectDefaultDtype():
+            dtype = self._opExportSlot.Input.meta.dtype
+            index = self.dtypeCombo.findData( dtype.__name__ )
+            self.dtypeCombo.setCurrentIndex( index )
+    
+        def _handleConvertDtypeChecked():
+            checked = self.convertDtypeCheckbox.isChecked()
+            self.dtypeCombo.setEnabled( checked )
+            if not checked:
+                _selectDefaultDtype()
+
+        self.convertDtypeCheckbox.toggled.connect( _handleConvertDtypeChecked )
         dtypes = collections.OrderedDict([ ( "unsigned byte",   "uint8" ),
                                            ( "unsigned 16-bit", "uint16" ),
                                            ( "unsigned 32-bit", "uint32" ),
@@ -142,9 +173,12 @@ class DataExportOptionsDlg(QDialog):
             self._opExportSlot.ExportDtype.setValue( dtype )
     
         self.dtypeCombo.currentIndexChanged.connect( _handleDtypeSelected )
-        self._selectDefaultDtype()
+        _selectDefaultDtype()
         self.dtypeCombo.setEnabled( False )
 
+    #**************************************************************************
+    # Renormalization
+    #**************************************************************************
     def _initRenormalizationWidgets(self):
         opExportSlot = self._opExportSlot
         dtype = opExportSlot.Input.meta.dtype
@@ -166,13 +200,18 @@ class DataExportOptionsDlg(QDialog):
             self.inputValueRange.setLimits( *dtype_limits(dtype) )
             self.inputValueRange.setValues( *drange )
 
+        def _updateOutputRange(output_dtype):
+            self.outputValueRange.setDType( output_dtype )
+            self.outputValueRange.setLimits( *dtype_limits( output_dtype ) )
+            self.outputValueRange.setValues( *default_drange( output_dtype ) )
+
         def _handleRenormalizeChecked( checked ):
             self.inputValueRange.setEnabled( checked )
             self.outputValueRange.setEnabled( checked )
             if checked:
                 output_dtype = opExportSlot.ImageToExport.meta.dtype
                 _setDefaultInputRange()
-                self._updateOutputRange(output_dtype)
+                _updateOutputRange(output_dtype)
                 _handleRangeChange()
             else:
                 # Clear the gui
@@ -187,13 +226,25 @@ class DataExportOptionsDlg(QDialog):
         self.inputValueRange.changedSignal.connect( _handleRangeChange )
         self.outputValueRange.changedSignal.connect( _handleRangeChange )
 
-        self._updateOutputRange( opExportSlot.ImageToExport.meta.dtype )
+        _updateOutputRange( opExportSlot.ImageToExport.meta.dtype )
         _handleRenormalizeChecked( self.renormalizeCheckbox.isChecked() )
         self.renormalizeCheckbox.toggled.connect( _handleRenormalizeChecked )
 
-        # Update the output range widget whenever the output dtype changes.
-        opExportSlot.ImageToExport.notifyMetaChanged( self._handleOutputDtypeChange )
+        def _handleOutputDtypeChange(*args):
+            """
+            The output slot dtype changed.
+            Update the normalization gui controls with the appropriate limits.
+            """
+            output_dtype = self._opExportSlot.ImageToExport.meta.dtype
+            if output_dtype != self.outputValueRange.dtype and self.renormalizeCheckbox.isChecked():
+                _updateOutputRange(output_dtype)
 
+        # Update the output range widget whenever the output dtype changes.
+        opExportSlot.ImageToExport.notifyMetaChanged( _handleOutputDtypeChange )
+
+    #**************************************************************************
+    # Axis order
+    #**************************************************************************
     def _initAxisOrderWidgets(self):
         def _handleNewAxisOrder():
             # Validator should ensure that this text is okay to use, 
@@ -211,65 +262,72 @@ class DataExportOptionsDlg(QDialog):
                 self._opExportSlot.OutputAxisOrder.disconnect()
                 self._updateAxisOrderColor(False)
         
-        self.outputAxisOrderEdit.setValidator( AxisOrderValidator( self, self._opExportSlot.ConvertedImage ) )
         self.outputAxisOrderEdit.editingFinished.connect( _handleNewAxisOrder )
         self.outputAxisOrderEdit.textChanged.connect( partial(self._updateAxisOrderColor, True) )
-        self.outputAxisOrderEdit.installEventFilter(self)
+        self.outputAxisOrderEdit.setValidator( DataExportOptionsDlg._AxisOrderValidator( self, self._opExportSlot.ConvertedImage ) )
+        self.outputAxisOrderEdit.installEventFilter( DataExportOptionsDlg._AxisOrderEventFilter(self) )
         self.axisOrderCheckbox.toggled.connect( _handleAxisOrderChecked )
 
     def _updateAxisOrderColor(self, allow_intermediate):
         checked = self.axisOrderCheckbox.isChecked()
         text = self.outputAxisOrderEdit.text()
         state, _ = self.outputAxisOrderEdit.validator().validate( text, 0 )
-        if checked and state != QValidator.Acceptable and not allow_intermediate:
+        invalidAxes = (checked and state != QValidator.Acceptable and not allow_intermediate)
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled( not invalidAxes )
+        if invalidAxes:
             self.outputAxisOrderEdit.setStyleSheet("QLineEdit {background-color: red}" )
         else:
             self.outputAxisOrderEdit.setStyleSheet("QLineEdit {background-color: white}" )
 
-    def _initFileOptionsWidget(self):
-        opExportSlot = self._opExportSlot
-        self.exportFileOptionsWidget.initExportOp( opExportSlot )
+    class _AxisOrderEventFilter(QObject):
+        def __init__(self, parent):
+            super( DataExportOptionsDlg._AxisOrderEventFilter, self ).__init__(parent)
 
-    def _handleOutputDtypeChange(self, *args):
-        """
-        The output slot dtype changed.
-        Update the normalization gui controls with the appropriate limits.
-        """
-        output_dtype = self._opExportSlot.ImageToExport.meta.dtype
-        if output_dtype != self.outputValueRange.dtype and self.renormalizeCheckbox.isChecked():
-            self._updateOutputRange(output_dtype)
-
-    def _updateOutputRange(self, output_dtype):
-        self.outputValueRange.setDType( output_dtype )
-        self.outputValueRange.setLimits( *dtype_limits( output_dtype ) )
-        self.outputValueRange.setValues( *default_drange( output_dtype ) )
-
-    def _selectDefaultDtype(self):
-        dtype = self._opExportSlot.Input.meta.dtype
-        index = self.dtypeCombo.findData( dtype.__name__ )
-        self.dtypeCombo.setCurrentIndex( index )
-
-    def _handleConvertDtypeChecked(self):
-        checked = self.convertDtypeCheckbox.isChecked()
-        self.dtypeCombo.setEnabled( checked )
-        if not checked:
-            self._selectDefaultDtype()
-        
-    def eventFilter(self, watched, event):
-        if watched == self and \
-           event.type() == QEvent.KeyPress and \
-           ( event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return):
-            # Ignore 'enter' keypress events.
-            # The user must manually click the 'OK' button to close the dialog.
-            return True
-        if watched == self.outputAxisOrderEdit:
+        def eventFilter(self, watched, event):
+            # Watch for focus-out events and 'enter' keypresses
             if event.type() == QEvent.FocusOut or \
                ( event.type() == QEvent.KeyPress and \
                  ( event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return) ):
-                self._updateAxisOrderColor( allow_intermediate=False )
+                self.parent()._updateAxisOrderColor( allow_intermediate=False )    
+            return False
 
-        return False
+    class _AxisOrderValidator(QValidator):
+        def __init__(self, parent, inputSlot):
+            super( DataExportOptionsDlg._AxisOrderValidator, self ).__init__(parent)
+            self._inputSlot = inputSlot
         
+        def validate(self, userAxes, pos):
+            taggedShape = self._inputSlot.meta.getTaggedShape()
+            inputAxes = taggedShape.keys()
+            inputSet = set(inputAxes)
+            userSet = set(str(userAxes))
+            
+            # Ensure all user axes appear in the input
+            if not (userSet <= inputSet):
+                return (QValidator.Invalid, pos)
+            
+            # Ensure no repeats
+            if len(userSet) != len(userAxes):
+                return (QValidator.Invalid, pos)
+            
+            # If missing non-singleton axes, maybe intermediate entry
+            # (It's okay to omit singleton axes)
+            for key in (inputSet - userSet):
+                if taggedShape[key] != 1:
+                    return (QValidator.Intermediate, pos)
+            
+            return (QValidator.Acceptable, pos)
+
+    #**************************************************************************
+    # File format and options
+    #**************************************************************************
+    def _initFileOptionsWidget(self):
+        opExportSlot = self._opExportSlot
+        self.exportFileOptionsWidget.initExportOp( opExportSlot )
+        
+#**************************************************************************
+# Helper functions
+#**************************************************************************
 def default_drange(dtype):
     if numpy.issubdtype(dtype, numpy.integer):
         return dtype_limits(dtype)
@@ -284,40 +342,13 @@ def dtype_limits(dtype):
         return (numpy.finfo(dtype).min, numpy.finfo(dtype).max)
     raise RuntimeError( "Unknown dtype: {}".format( dtype ) )
 
-class AxisOrderValidator(QValidator):
-    def __init__(self, parent, inputSlot):
-        super( AxisOrderValidator, self ).__init__(parent)
-        self._inputSlot = inputSlot
-    
-    def validate(self, userAxes, pos):
-        taggedShape = self._inputSlot.meta.getTaggedShape()
-        inputAxes = taggedShape.keys()
-        inputSet = set(inputAxes)
-        userSet = set(str(userAxes))
-        
-        # Ensure all user axes appear in the input
-        if not (userSet <= inputSet):
-            return (QValidator.Invalid, pos)
-        
-        # Ensure no repeats
-        if len(userSet) != len(userAxes):
-            return (QValidator.Invalid, pos)
-        
-        # If missing non-singleton axes, maybe intermediate entry
-        # (It's okay to omit singleton axes)
-        for key in (inputSet - userSet):
-            if taggedShape[key] != 1:
-                return (QValidator.Intermediate, pos)
-        
-        return (QValidator.Acceptable, pos)
-
-
-
+#**************************************************************************
+# Quick debug
+#**************************************************************************
 if __name__ == "__main__":
-    import numpy
     import vigra
-    from PyQt4.QtGui import QApplication, QSpinBox
-    from lazyflow.graph import Graph, Operator, InputSlot
+    from PyQt4.QtGui import QApplication
+    from lazyflow.graph import Graph
     from lazyflow.operators.ioOperators import OpFormattedDataExport
 
     data = numpy.zeros( (10,20,30,3), dtype=numpy.float32 )
