64b0bf794abac616d51ca19d325dc165c802cfa3
Christoph Decker <christoph.decker@iwr.uni-heidelberg.de>
2014-07-24 17:45:49 +0200

    Wysiwyg export working properly in all tested ilastik applets

--- volumina/widgets/wysiwygExportOptionsDlg.py
+++ volumina/widgets/wysiwygExportOptionsDlg.py
@@ -20,11 +20,13 @@
 #		   http://ilastik.org/license/
 ###############################################################################
 import os
-import vigra
+import numpy as np
 
 from PyQt4 import uic
-from PyQt4.QtCore import Qt, QEvent
-from PyQt4.QtGui import QDialog, QFileDialog
+from PyQt4.QtCore import Qt, QEvent, QString, QRectF
+from PyQt4.QtGui import QDialog, QDialogButtonBox, QFileDialog, QImageWriter, QImage, QPainter
+
+from volumina.widgets.multiStepProgressDialog import MultiStepProgressDialog
 
 class WysiwygExportOptionsDlg(QDialog):
     
@@ -32,29 +34,33 @@ class WysiwygExportOptionsDlg(QDialog):
         """
         Constructor.
         
-        :param parent: The parent widget
+        :param view: The parent widget -> ImageView2D
         """
-        super( WysiwygExportOptionsDlg, self ).__init__(view.parent())
+        super( WysiwygExportOptionsDlg, self ).__init__(view)
         uic.loadUi( os.path.splitext(__file__)[0] + '.ui', self )
 
-        # references
-        self.parent = view.parent()
         self.view = view
-        self.scene = view.scene()
         
+        # indicators for ok button
+        self._pattern_ok = False
+        self._directory_ok = False
+
         # properties
-        self.along = self.scene._along
+        self.along = self.view.scene()._along
         self.inputAxes = ['t','x','y','z','c']
-        self.shape = self.scene._posModel.shape5D
+        self.shape = self.view.scene()._posModel.shape5D
         self.sliceAxes = [i for i in range(len(self.inputAxes)) if not i in self.along]
         self.sliceCoords = ''.join([a for i,a in enumerate(self.inputAxes) 
-                                    if not i in self.along]).upper()
+                                    if not i in self.along])
 
         # Init child widgets
-        self._initFileOptionsWidget()
         self._initSubregionWidget()
-        self._initMetaInfoWidgets()
-
+        self._initFileOptionsWidget()
+        self._initExportInfoWidget()
+        
+        # disable OK button if file path/pattern are invalid
+        self._updateOkButton()
+        
         # See self.eventFilter()
         self.installEventFilter(self)
 
@@ -66,65 +72,40 @@ class WysiwygExportOptionsDlg(QDialog):
            ( event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return):
             return True
         return False
-
-    def _initMetaInfoWidgets(self):
+        
+    def getRoi(self):
+        # return roi with 'real' numbers instead of 'None' for full range
+        self.roi_start = tuple([s if not s is None else 0 for s in self.roi_start])
+        self.roi_stop = tuple([s if not s is None else self.shape[i] 
+                      for i,s in enumerate(self.roi_stop)])
+        return self.roi_start, self.roi_stop
+    
+    def getIterAxes(self):
+        # return axes to iterate over (e.g. time t) in format: axes indices, axes symbols, stack size
+        start, stop = self.getRoi()
+        axes = [i for i in self.along if stop[i]-start[i] > 1]
+        return axes, [self.inputAxes[i] for i in axes], [stop[i]-start[i] for i in axes]
+    
+    def getExportInfo(self):
+        # return directory, file name pattern and extension
+        return str(self.directoryEdit.text()), self.filePattern, self.fileExt
+    
+    def showMarkers(self):
+        return self.displayMarkersCheckbox.isChecked()
+    
+    def _initExportInfoWidget(self):
         self._updateExportDesc()
-        self._updateFilePattern()
-    
-    def _updateFilePattern(self):
-        # if iterator axes change, update file pattern accordingly
-        _, co, _ = self.getIterAxes()
-        iters = "".join(["_%s={%s}" % (c, c.lower()) for c in co])
-        txt = "export" + iters
-        self.filePattern = txt
-        self.filePatternEdit.setText(txt + "." + self.fileExt)
-        self.filePatternInvalidLabel.setVisible(False)
-        return txt
-        
-    def _validateFilePattern(self, txt):
-        # check if placeholders for iterator axes are there (e.g. {t})
-        _, co, _ = self.getIterAxes()
-        valid = len([1 for c in co if not ("{%s}" % c.lower()) in txt]) < 1
-        if valid:
-            self.filePatternInvalidLabel.setVisible(False)
-        else:
-            plc = ", ".join([("{%s}" % c.lower()) for c in co])
-            txt = "File pattern invalid! Pattern has to contain placeholder(s) %s." % plc
-            self.filePatternInvalidLabel.setText('<font color="red">'+txt+'</font>')
-            self.filePatternInvalidLabel.setVisible(True)
-        return valid
-    
-    def _updateExportDesc(self):
-        # if iterator axes or slice shape changes, update export description accordingly
-        ax, co, it = self.getIterAxes()
-        w = self.roi_stop[self.sliceAxes[0]] - self.roi_start[self.sliceAxes[0]]
-        h = self.roi_stop[self.sliceAxes[1]] - self.roi_start[self.sliceAxes[1]]
-        
-        # describe stacking
-        slice_str = "images"
-        if len(ax) == 1:
-            iter_desc = " (stacked along %s-direction)" % co[0]
-            iter_num = "%d " % it[0]
-        elif len(ax) == 2:
-            iter_desc= (" (%d along %s-direction times "
-                        "%d along %s-direction)" % (it[0], co[0], it[1], co[1]))
-            iter_num = "%d " % it[0]*it[1]
-        else:
-            iter_desc = ""
-            iter_num = "1 "
-            slice_str = "image"
-        
-        # write full description to label in widget
-        desc = iter_num + self.sliceCoords + "-" + slice_str + iter_desc + " of size %d x %d" % (w,h)
-        self.exportDesc.setText(desc)
     
     def _initSubregionWidget(self):
-        # initialize roi which spans everything
+        # initialize roi for current viewport
         start = [None,] * len(self.shape)
         stop = [None,] * len(self.shape)
-        
-        # fill in start and stop values from current viewport rectangle
+        pos5d = self.view.scene()._posModel.slicingPos5D
         rect = self.view.viewportRect()
+        for i in range(4):
+            if self.shape[i] > 1:
+                start[i] = pos5d[i]
+                stop[i] = pos5d[i]+1
         start[self.sliceAxes[0]] = rect.left()
         stop[self.sliceAxes[0]] = rect.right()
         start[self.sliceAxes[1]] = rect.top()
@@ -135,21 +116,21 @@ class WysiwygExportOptionsDlg(QDialog):
         self.roi_stop = tuple(stop)
         
         # initialize widget
-        self.roiWidget.initWithExtents(self.inputAxes, self.shape, 
-                                       self.roi_start, self.roi_stop)
+        self.roiWidget.initWithExtents(self.inputAxes[:-1], self.shape[:-1], 
+                                       self.roi_start[:-1], self.roi_stop[:-1])
         
         # if user changes roi in widget, save new values to class        
         def _handleRoiChange(newstart, newstop):
-            self.roi_start = newstart
-            self.roi_stop = newstop
+            self.roi_start = newstart+(None,)
+            self.roi_stop = newstop+(None,)
             self._updateExportDesc()
             self._updateFilePattern()
 
         self.roiWidget.roiChanged.connect(_handleRoiChange)
 
-    def _initFileOptionsWidget(self):
-        # List all supported formats
-        exts = vigra.impex.listExtensions().split()
+    def _initFileOptionsWidget(self):        
+        # List all image formats supported by QImageWriter
+        exts = [str(QString(ext)) for ext in list(QImageWriter.supportedImageFormats())]
         
         # insert them into file format combo box
         for ext in exts:
@@ -158,11 +139,67 @@ class WysiwygExportOptionsDlg(QDialog):
         # connect handles
         self.fileFormatCombo.currentIndexChanged.connect(self._handleFormatChange)
         self.filePatternEdit.textEdited.connect(self._handlePatternChange)
-        self.selectDirectoryButton.clicked.connect(self._browseForFilepath)
+        self.directoryEdit.textChanged.connect(self._validateFilePath)
+        self.selectDirectoryButton.clicked.connect(self._browseForPath)
             
         # set default file format to png
         self.fileFormatCombo.setCurrentIndex(exts.index('png'))
+        self._updateFilePattern()
+        self._validateFilePath()
+    
+    def _updateExportDesc(self):
+        # if iterator axes or slice shape changes, update export description accordingly
+        ax, co, it = self.getIterAxes()
+        w = self.roi_stop[self.sliceAxes[0]] - self.roi_start[self.sliceAxes[0]]
+        h = self.roi_stop[self.sliceAxes[1]] - self.roi_start[self.sliceAxes[1]]
         
+        # describe stacking
+        slice_str = "images"
+        if len(ax) == 1:
+            iter_desc = " (stacked along %s-direction)" % co[0].upper()
+            iter_num = "%d " % it[0]
+        elif len(ax) == 2:
+            iter_desc= (" (%d along %s-direction times "
+                        "%d along %s-direction)" % (it[0], co[0].upper(), it[1], co[1].upper()))
+            iter_num = "%d " % it[0]*it[1]
+        else:
+            iter_desc = ""
+            iter_num = "1 "
+            slice_str = "image"
+        
+        # write full description to label in widget
+        desc = iter_num + self.sliceCoords.upper() + "-" + slice_str + iter_desc + " of size %d x %d" % (w,h)
+        self.exportDesc.setText(desc)
+    
+    def _updateFilePattern(self):
+        # if iterator axes change, update file pattern accordingly
+        _, co, _ = self.getIterAxes()
+        iters = "".join(["_%s={%s}" % (c.upper(), c) for c in co])
+        txt = "export" + iters
+        self.filePattern = txt
+        self.filePatternEdit.setText(txt + "." + self.fileExt)
+        self._validateFilePattern(txt)
+        return txt
+    
+    def _validateFilePattern(self, txt):
+        # check if placeholders for iterator axes are there (e.g. {t})
+        _, co, _ = self.getIterAxes()
+        valid = len([1 for c in co if not ("{%s}" % c.lower()) in txt]) < 1
+        if valid:
+            self.filePatternInvalidLabel.setVisible(False)
+            self._pattern_ok = True
+        else:
+            plc = ", ".join([("{%s}" % c.lower()) for c in co])
+            txt = "File pattern invalid! Pattern has to contain placeholder(s) %s." % plc
+            self.filePatternInvalidLabel.setText('<font color="red">'+txt+'</font>')
+            self.filePatternInvalidLabel.setVisible(True)
+            self._pattern_ok = False
+        self._updateOkButton()
+        return valid
+    
+    def _updateOkButton(self):
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(self._pattern_ok and self._directory_ok)
+    
     def _updateFileExtensionInPattern(self, ext):
         fname = str(self.filePatternEdit.text()).split(".")
         if len(fname) > 1:
@@ -170,11 +207,10 @@ class WysiwygExportOptionsDlg(QDialog):
         else:
             txt = ".".join(fname+[ext,])
         self.filePatternEdit.setText(txt)
-            
+        
     def _handleFormatChange(self):
         self.fileExt = str(self.fileFormatCombo.currentText()).split(" ")[0]
         self._updateFileExtensionInPattern(self.fileExt)
-        print self.fileExt
         
     def _handlePatternChange(self):
         txt = str(self.filePatternEdit.text()).split(".")
@@ -188,26 +224,183 @@ class WysiwygExportOptionsDlg(QDialog):
         txt = ".".join(txt[:-1])
         if self._validateFilePattern(txt):
             self.filePattern = txt
-        print self.filePattern
         
-    def _browseForFilepath(self):
-        starting_dir = os.path.expanduser("~")
-        export_dir = QFileDialog.getExistingDirectory(self, "Export Directory", starting_dir)
-        
+    def _browseForPath(self):
+        export_dir = QFileDialog.getExistingDirectory(self, "Export Directory", 
+                                                      os.path.expanduser("~"))
         if not export_dir.isNull():
             self.directoryEdit.setText(export_dir)
+            
+    def _validateFilePath(self):
+        txt = str(self.directoryEdit.text())
+        # check if txt is a valid directory and writable
+        valid = os.path.isdir(txt) and os.access(txt, os.W_OK)
+        if valid:
+            self.directoryInvalidLabel.setVisible(False)
+            self._directory_ok = True
+        else:
+            txt = "Directory invalid! Please select a writable directory."
+            self.directoryInvalidLabel.setText('<font color="red">'+txt+'</font>')
+            self.directoryInvalidLabel.setVisible(True)
+            self._directory_ok = False
+        self._updateOkButton()
+        return valid
+
+# this is not a lazyflow operator on purpose -> it's supposed to work with volumina only
+class WysiwygExportHelper(MultiStepProgressDialog):
+    """
+    MultiStepProgressDialog capable of handling the WYSIWYG export including canceling
+    """
         
-    def getRoi(self):
-        self.roi_start = tuple([s if not s is None else 0 for s in self.roi_start])
-        self.roi_stop = tuple([s if not s is None else self.shape[i] 
-                      for i,s in enumerate(self.roi_stop)])
-        return self.roi_start, self.roi_stop
-    
-    def getIterAxes(self):
-        start, stop = self.getRoi()
-        axes = [i for i in self.along if stop[i]-start[i] > 1]
-        return axes, [self.inputAxes[i].upper() for i in axes], [stop[i]-start[i] for i in axes]
-    
-    def getExportInfo(self):
-        return str(self.directoryEdit.text()), self.filePattern, self.fileExt
+    def __init__(self, view, settingsDlg):
+        MultiStepProgressDialog.__init__(self, view)
         
+        self.view = view
+        self.posModel = view.scene()._posModel
+        self.dlg = settingsDlg
+        
+        self.exportloop = None
+        self.timer = None
+        
+        # connect signals
+        self.buttonBox.button(QDialogButtonBox.Cancel).clicked.connect(self.cancel)
+    
+    def run(self):
+        # disable Ok button until export is finished
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+        
+        # start exporting
+        self.timer = self.startTimer(0)   
+        
+        # show dialog     
+        MultiStepProgressDialog.exec_(self)
+    
+    def prepareExport(self):
+        if self.dlg is None:
+            return
+        
+        self.setNumberOfSteps(1)
+        
+        # grab settings from dialog
+        start, stop = self.dlg.getRoi()
+        iter_axes, iter_coords, iter_n = self.dlg.getIterAxes()
+        slice_axes = self.dlg.sliceAxes
+        show_markers = self.dlg.showMarkers()
+        folder, pattern, fileExt = self.dlg.getExportInfo()
+        
+        # width and height of images
+        w = stop[slice_axes[0]] - start[slice_axes[0]]
+        h = stop[slice_axes[1]] - start[slice_axes[1]]
+
+        # scene rectangle to render
+        rect = QRectF(start[slice_axes[0]], start[slice_axes[1]], w, h)
+        
+        # remember current position to correctly place view afterwards
+        self.currentPos5D = list(self.posModel.slicingPos5D)
+        pos = list(start)
+        
+        # show/hide slice intersection markers
+        self.showed_markers = self.view._sliceIntersectionMarker.isVisible()
+        if show_markers:
+            self.view._sliceIntersectionMarker.setVisible(True)
+            # to correctly display slice intersection markers
+            for a in slice_axes: 
+                pos[a] = self.currentPos5D[a]
+        else:            
+            self.view._sliceIntersectionMarker.setVisible(False)
+
+        # create plain image and painter
+        self.img = QImage(w, h, QImage.Format_RGB16)
+        self.painter = QPainter(self.img)
+
+        # calculate number of iterations/images to export
+        if len(iter_axes) > 0:
+            self.n_iter = np.prod(np.array(iter_n))
+        else:
+            self.n_iter = 1
+                        
+        # prepare export loop
+        self.exportloop = self.loopGenerator(rect, pos, start, stop, iter_axes, 
+                                             iter_coords, folder, pattern, fileExt)
+
+    # Idea from: http://stackoverflow.com/a/7226877
+    def loopGenerator(self, rect, pos, start, stop, iter_axes, 
+                      iter_coords, folder, pattern, fileExt):
+        if len(iter_axes) < 1:
+            # single image
+            fname = self._filename(folder, pattern, fileExt, iter_coords, [])
+            self._saveImg(pos, rect, fname)
+            self._setProgress(1)
+            yield
+        else:
+            k = 0
+            # iterate over first coordinate (e.g. t)
+            for i in range(start[iter_axes[0]], stop[iter_axes[0]]):
+                pos[iter_axes[0]] = i
+                
+                # iterate over second coordinate (e.g. z) if any
+                if len(iter_axes) > 1:
+                    for j in range(start[iter_axes[1]], stop[iter_axes[1]]):
+                        pos[iter_axes[1]] = j
+                        fname = self._filename(folder, pattern, fileExt, iter_coords, [i,j,])
+                        self._saveImg(pos, rect, fname)
+                        k += 1
+                        self._setProgress(k)
+                        yield
+                else:
+                    fname = self._filename(folder, pattern, fileExt, iter_coords, [i,])
+                    self._saveImg(pos, rect, fname)
+                    k += 1
+                    self._setProgress(k)
+                    yield
+
+    def cancel(self):
+        # kill timer
+        if self.timer is not None:
+            self.killTimer(self.timer)
+        self.exportloop = None
+        self.timer = None
+        self.cleanUp()
+        
+    def cleanUp(self):
+        # re-enable ok/finish button
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
+        
+        # close painter
+        self.painter.end()
+        
+        # reset viewer position
+        self._setPos5D(self.currentPos5D)
+        self.view._sliceIntersectionMarker.setVisible(self.showed_markers)
+
+    def timerEvent(self, event):
+        if not self.exportloop is None:
+            try:
+                # if not canceled, process next image in export loop
+                next(self.exportloop)
+            except StopIteration:
+                # if loop finished, call cancel to wrap up the process
+                self.cancel()
+
+    # convenience functions
+    def _setProgress(self, k):
+        self.setStepProgress(100*float(k)/self.n_iter)
+    
+    def _setPos5D(self, pos5d):
+        self.posModel.time = pos5d[0]
+        self.posModel.slicingPos = pos5d[1:4]
+        self.posModel.channel = pos5d[4]
+    
+    def _saveImg(self, pos, rect, fname):
+        #img.fill(0)
+        self._setPos5D(pos)
+        self.view.scene().joinRenderingAllTiles(viewport_only=False, rect=rect)
+        self.view.scene().render(self.painter, source=rect)
+        self.img.save(fname)
+    
+    def _filename(self, folder, pattern, extension, iters, coords):
+        for i, c in enumerate(iters):
+            to_rep = "{%s}" % c
+            pattern = pattern.replace(to_rep, str(coords[i]))
+        return os.path.join(folder, pattern+"."+extension)
+    
