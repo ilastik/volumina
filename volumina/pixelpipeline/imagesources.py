0f86ea3a22985425c75bf82a0f2ebf93aeda906e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-06 17:59:08 -0500

    volumina/pixelpipeline/imagesources.py: Ensure the first colortable entry is transparent no matter what.

--- volumina/pixelpipeline/imagesources.py
+++ volumina/pixelpipeline/imagesources.py
@@ -507,8 +507,8 @@ class ColortableImageRequest( object ):
                         # Copy the rest of the colors over.
                         _colorTable[1:] = self._colorTable
 
-                        # Make sure the first color is transparent.
-                        _colorTable[0] = 0
+                    # Make sure the first color is transparent.
+                    _colorTable[0] = 0
 
                 # Make masked values transparent.
                 a = np.ma.filled(a, 0)
