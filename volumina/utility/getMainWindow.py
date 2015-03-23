711fb4ac4576468af35b26443615b5e36fb575fb
Stuart Berg <bergs@janelia.hhmi.org>
2015-03-23 15:41:34 -0400

    utility.getMainWindow(): Fix heuristic (fixes keyboard shortcuts)

--- volumina/utility/getMainWindow.py
+++ volumina/utility/getMainWindow.py
@@ -3,32 +3,26 @@ from PyQt4.QtGui import QApplication, QWidget, QMainWindow
 def getMainWindow():
     """
     Attempt to return the main window for the app.
-    Since any QWidget could be the main window, we have to use some heuristics:
-    - If there's just one top-level widget, bingo.
-    - If one of the top-level widgets happens to be a QMainWindow, use that.
-    - If not, select the largets top-level widget
+    There's no guaranteed way to find *the* main window, 
+    so we use some heuristics and make a guess:
+    - Prefer instances of QMainWindow
+    - Prefer the biggest window without a parent.
     """
     top_level_widgets = list(QApplication.instance().topLevelWidgets())
-    
-    # If there's just one, return it.
-    if len(top_level_widgets) == 1:
-        return top_level_widgets[0]
-    
-    # If there's more than one, check for a QMainWindow
-    for widget in top_level_widgets:
-        if isinstance(widget, QMainWindow):
-            return widget
-    
-    # Otherwise, return the biggest one
+
+    # Real widgets only.
     top_level_widgets = filter( lambda w: isinstance(w, QWidget), top_level_widgets )
-    if len(top_level_widgets) == 0:
+
+    if not top_level_widgets:
+        # Couldn't find any
         return None
+    
+    # We prefer QMainWindow instances.  If we have any, drop all the other widgets.
+    main_windows = filter( lambda w: isinstance(w, QMainWindow), top_level_widgets )
+    if main_windows:
+        top_level_widgets = main_windows
 
-    biggest_size = top_level_widgets[0].size()
-    biggest_widget = top_level_widgets[0]
-    for widget in top_level_widgets[1:]:
-        if widget.size() > biggest_size:
-            biggest_size = widget.size()
-            biggest_widget = widget
-
+    # Now return the biggest widget we found.
+    sizes = map( lambda w: w.width() * w.height(), main_windows )
+    biggest_widget = max(zip(sizes, main_windows))[1]
     return biggest_widget
