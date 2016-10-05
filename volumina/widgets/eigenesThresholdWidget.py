e223666ff3176cdcf2c8a9977ab3960986c11746
A. Haller <shaker6691@hotmail.de>
2016-10-05 16:38:18 +0200

    eigenesThreshold

---
+++ volumina/widgets/eigenesThresholdWidget.py
@@ -0,0 +1,110 @@
+###############################################################################
+#   volumina: volume slicing and editing library
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the Lesser GNU General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# See the files LICENSE.lgpl2 and LICENSE.lgpl3 for full text of the
+# GNU Lesser General Public License version 2.1 and 3 respectively.
+# This information is also available on the ilastik web site at:
+#		   http://ilastik.org/license/
+###############################################################################
+#Python
+from os import path
+
+#PyQt
+from PyQt4 import uic
+from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtGui import QWidget, QButtonGroup
+
+#===----------------------------------------------------------------------------------------------------------------===
+
+class EigenesThresholdWidget(QWidget):
+    valueChanged = pyqtSignal(int, int)
+    
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+            
+        p = path.split(__file__)[0]
+        if not p:
+            p = "."
+        
+        uic.loadUi(p+"/ui/eigenesThresholdWidget.ui", self)
+        self.setRange(0,255)
+        
+        self._minSlider.valueChanged.connect(self._onMinSliderMoved)
+        self._maxSlider.valueChanged.connect(self._onMaxSliderMoved)
+        self.checkBox.stateChanged.connect(self._onCheckboxClicked)
+    
+    def _onMinSliderMoved(self, v):
+        if v >= self._maxSlider.value():
+            if v < self._maxSlider.maximum():
+                self._maxSlider.setValue(v+1)
+            else:
+                self._minSlider.setValue(v-1)
+        self.valueChanged.emit(self._minSlider.value(), self._maxSlider.value())
+    
+    def _onMaxSliderMoved(self, v):
+        if v <= self._minSlider.value():
+            if v > self._minSlider.minimum():
+                self._minSlider.setValue(v-1)
+            else:
+                self._maxSlider.setValue(self._minSlider.minimum()+1)
+        self.valueChanged.emit(self._minSlider.value(), self._maxSlider.value())
+
+    def _onCheckboxClicked(self):
+        if (self.checkBox.isChecked()):
+            print "checked"
+            self._minSlider.setEnabled(True)
+            self._maxSlider.setEnabled(True)
+            self._minSpin.setEnabled(True)
+            self._maxSpin.setEnabled(True)
+        else:
+            print "not checked"
+            self._minSlider.setEnabled(False)
+            self._maxSlider.setEnabled(False)
+            self._minSpin.setEnabled(False)
+            self._maxSpin.setEnabled(False)
+
+    
+    def setLayername(self, n):
+        self._layerLabel.setText("Layer <b>%s</b>" % n)
+        
+    def setRange(self, minimum, maximum):
+        self._minSlider.setRange(minimum, maximum)
+        self._minSpin.setRange(minimum, maximum)
+        self._maxSlider.setRange(minimum, maximum)
+        self._maxSpin.setRange(minimum, maximum)
+        self._minSpin.setSuffix("/%d" % maximum)
+        self._maxSpin.setSuffix("/%d" % maximum)
+        self._minSlider.setValue(minimum)
+        self._maxSlider.setValue(maximum)
+        self._minSpin.setValue(minimum)
+        self._maxSpin.setValue(maximum)
+
+    def setValue(self, minimum, maximum):
+        self._minSlider.setValue(minimum)
+        self._maxSlider.setValue(maximum)
+        self._minSpin.setValue(minimum)
+        self._maxSpin.setValue(maximum)
+        
+#===----------------------------------------------------------------------------------------------------------------===
+#=== __name__ == "__main__"                                                                                         ===
+#===----------------------------------------------------------------------------------------------------------------===
+
+if __name__ == "__main__":
+    from PyQt4.QtGui import QApplication
+    app = QApplication([])
+    w = EigenesThresholdWidget()
+    w.show()
+    app.exec_()
