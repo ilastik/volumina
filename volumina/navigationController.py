9a80117f5362423f15a358415e1c6b8f1481cfb3
Janez Ales <janez.ales@iwr.uni-heidelberg.de>
2017-09-21 15:49:44 +0200

    we need _default for left click also and because there is nothing specific to the RightClick in onMousePressRight_default we can use it for all mouse clicks that are not handled with higher priority in the if statement
    type of the mouse click is passed via event parameter
    
    (cherry picked from commit 622389519cdc3b9b9c7baaf4f62c7824151a7a38)

--- volumina/navigationController.py
+++ volumina/navigationController.py
@@ -97,10 +97,9 @@ class NavigationInterpreter(QObject):
             elif etype == QEvent.MouseButtonDblClick:
                 return self.onMouseDoubleClick_default( watched, event )
 
-            elif etype == QEvent.MouseButtonPress and event.button() == Qt.RightButton:
-                return self.onMousePressRight_default( watched, event )
-            
-            
+            elif etype == QEvent.MouseButtonPress:
+                return self.onMousePress_default( watched, event )
+
         elif self._current_state == self.DRAG_MODE:
             ### drag mode -> default mode
             if etype == QEvent.MouseButtonRelease:
@@ -195,7 +194,7 @@ class NavigationInterpreter(QObject):
 
         self._navCtrl.positionDataCursor(QPointF(dataX, dataY), self._navCtrl._views.index(imageview))
 
-    def onMousePressRight_default( self, imageview, event ):
+    def onMousePress_default( self, imageview, event ):
         #make sure that we have the cursor at the correct position
         #before we call the context menu
         self.onMouseMove_default( imageview, event )
