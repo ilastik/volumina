afeba0af857db30fc86a754524a2e8759d6e85e2
Maksim Novikov <mnovikov.work@gmail.com>
2019-08-20 14:35:10 +0200

    Cleanup datasources imports

--- volumina/pixelpipeline/datasources/__init__.py
+++ volumina/pixelpipeline/datasources/__init__.py
@@ -1,10 +1,7 @@
-from __future__ import print_function
-from __future__ import absolute_import
-
 ###############################################################################
 #   volumina: volume slicing and editing library
 #
-#       Copyright (C) 2011-2014, the ilastik developers
+#       Copyright (C) 2011-2019, the ilastik developers
 #                                <team@ilastik.org>
 #
 # This program is free software; you can redistribute it and/or
@@ -22,25 +19,23 @@ from __future__ import absolute_import
 # This information is also available on the ilastik web site at:
 # 		   http://ilastik.org/license/
 ###############################################################################
-import sys
-import threading
-import logging
-import weakref
-from functools import partial, wraps
-from PyQt5.QtCore import QObject, pyqtSignal, QTimer
-from volumina.pixelpipeline.asyncabcs import RequestABC, SourceABC, IndeterminateRequestError
-import volumina
-from volumina.slicingtools import is_pure_slicing, slicing2shape, is_bounded, make_bounded, index2slice, sl
-from volumina.config import CONFIG
-import numpy as np
-from future.utils import raise_with_traceback
-
 from .array import ArraySource, ArraySinkSource, RelabelingArraySource
 from .constant import ConstantSource
 from .minmax import MinMaxSource
 from .halo import HaloAdjustedDataSource
 
+__all__ = [
+    "ArraySource",
+    "ArraySinkSource",
+    "RelabelingArraySource",
+    "ConstantSource",
+    "MinMaxSource",
+    "HaloAdjustedDataSource",
+]
+
 try:
     from .lazyflow import LazyflowSource, LazyflowSinkSource
+
+    __all__ += ["LazyflowSource", "LazyflowSinkSource"]
 except ImportError:
     pass
