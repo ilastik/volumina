6977e175ee85ce53c731d158b6cccb3e91c2f28f
Tomaz Vieira <tom.fogaca@gmail.com>
2020-07-21 09:40:30 +0200

    Simplifying the code a bit more

--- volumina/widgets/layerDialog.py
+++ volumina/widgets/layerDialog.py
@@ -32,6 +32,8 @@ from functools import partial
 from typing import Callable
 from pathlib import Path
 
+from volumina.widgets.thresholdingWidget import ThresholdingWidget
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -46,27 +48,22 @@ class LayerDialog(QDialog):
         self.setLayername(layer.name)
         self.layer = layer
 
-    def update_widget_limits(self, datasourceIdx: int, thresholding_widget, autorange_widget):
-        thresholding_widget.setRange(self.layer.range[datasourceIdx][0], self.layer.range[datasourceIdx][1])
+    def initialize_range_widgets(self, datasourceIdx: int, thresholding_widget: ThresholdingWidget, autorange_checkbox):
+        def handleRangeChanged(a, b):
+            self.layer.set_normalize(datasourceIdx, (a, b))
+            print(f"normalization  aeeee changed for channel={datasourceIdx} to [{a}, {b}]")
+
         normalize_range = self.layer.get_datasource_range(datasourceIdx)
-        thresholding_widget.setValue(normalize_range[0], normalize_range[1])
+        thresholding_widget.setRange(normalize_range[0], normalize_range[1])
+        thresholding_widget.valueChanged.connect(handleRangeChanged)
 
-        autorange_widget.stateChanged.connect(
-            self.make_range_checkbox_handler(datasourceIdx=0, thresholding_widget=thresholding_widget)
-        )
-        autorange_widget.setCheckState(layer._autoMinMax[datasourceIdx] * 2)
+        def handleAutoRangeChanged(state):
+            self.layer.set_normalize(datasourceIdx, None if state == Qt.Checked else thresholding_widget.getRange())
+            thresholding_widget.setEnabled(state == Qt.Unchecked)
 
-    def make_range_checkbox_handler(self, datasourceIdx: int, thresholding_widget) -> Callable[[int], None]:
-        def autoRangeHandler(state):
-            if state == Qt.Checked:
-                self.update_widget_limits(datasourceIdx=datasourceIdx, thresholding_widget=thresholding_widget)
-                self.layer.set_normalize(datasourceIdx, None)  # set to auto
-                thresholding_widget.setEnabled(False)
-            if state == Qt.Unchecked:
-                thresholding_widget.setEnabled(True)
-                self.layer.set_normalize(datasourceIdx, self.layer.normalize[datasourceIdx])
-
-        return autoRangeHandler
+        autorange_checkbox.stateChanged.connect(handleAutoRangeChanged)
+        autorange_state = Qt.Checked if self.layer._autoMinMax[datasourceIdx] else Qt.Unchecked
+        autorange_checkbox.setCheckState(autorange_state)
 
     def setLayername(self, name: str):
         self._layerLabel.setText(f"<b>{name}</b>")
@@ -76,44 +73,29 @@ class GrayscaleLayerDialog(LayerDialog):
     def __init__(self, layer, parent=None):
         super().__init__(ui_file_name="grayLayerDialog.ui", layer=layer, parent=parent)
 
-        def dbgPrint(a, b):
-            layer.set_normalize(0, (a, b))
-            logger.debug("normalization changed to [%d, %d]" % (a, b))
-
-        # import pydevd; pydevd.settrace()
-        self.update_widget_limits(
-            datasourceIdx=0, thresholding_widget=self.grayChannelThresholdingWidget, autorange_widget=self.grayAutoRange
+        self.initialize_range_widgets(
+            datasourceIdx=0,
+            thresholding_widget=self.grayChannelThresholdingWidget,
+            autorange_checkbox=self.grayAutoRange,
         )
-        self.grayChannelThresholdingWidget.valueChanged.connect(dbgPrint)
 
 
 class RGBALayerDialog(LayerDialog):
     def __init__(self, layer, parent=None):
         super().__init__(ui_file_name="rgbaLayerDialog.ui", layer=layer, parent=parent)
 
-        def dbgPrint(layerIdx, a, b):
-            layer.set_normalize(layerIdx, (a, b))
-            logger.debug("normalization changed for channel=%d to [%d, %d]" % (layerIdx, a, b))
-
-        thresholding_widgets = [
-            self.redChannelThresholdingWidget,
-            self.greenChannelThresholdingWidget,
-            self.blueChannelThresholdingWidget,
-            self.alphaChannelThresholdingWidget,
-        ]
-        auto_range_widgets = [self.redAutoRange, self.greenAutoRange, self.blueAutoRange, self.alphaAutoRange]
-        channels = [self.redChannel, self.greenChannel, self.blueChannel, self.alphaChannel]
-
-        for idx, t_widget in enumerate(thresholding_widgets):
-            channel = channels[idx]
-            range_widget = auto_range_widgets[idx]
-
+        for idx, (t_widget, autorange_checkbox, channel) in enumerate(
+            [
+                (self.redChannelThresholdingWidget, self.redAutoRange, self.redChannel),
+                (self.greenChannelThresholdingWidget, self.greenAutoRange, self.greenChannel),
+                (self.blueChannelThresholdingWidget, self.blueAutoRange, self.blueChannel),
+                (self.alphaChannelThresholdingWidget, self.alphaAutoRange, self.alphaChannel),
+            ]
+        ):
             if layer.datasources[idx] == None:
                 channel.setVisible(False)
                 continue
-
-            self.update_widget_limits(idx, thresholding_widget=t_widget, autorange_widget=range_Widget)
-            t_widget.valueChanged.connect(partial(dbgPrint, idx))
+            self.initialize_range_widgets(idx, thresholding_widget=t_widget, autorange_checkbox=autorange_checkbox)
 
         self.resize(self.minimumSize())
 
