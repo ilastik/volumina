8cdbe2acc133a88aa99e63697d2dacbad6a817c8
Emil Melnikov <emilmelnikov@gmail.com>
2020-10-16 12:21:52 +0200

    Update tests
    Make sure that the old preferences file still exists after migration.
    Also test migeation from invalid old data instead of noop test.

--- tests/test_utility/test_preferences.py
+++ tests/test_utility/test_preferences.py
@@ -35,23 +35,21 @@ def test_set_path(tmp_path, preferences):
 
 def test_migrate(tmp_path, preferences):
     data = {"s1": {"k1": "spam"}, "s2": {"k2": 42, "k3": True, "k4": 0.42, "k5": None}}
-
     old_path = tmp_path / "old_preferences.pickle"
-    with open(old_path, "wb") as f:
-        pickle.dump(data, f)
+    old_path.write_bytes(pickle.dumps(data))
 
     preferences.migrate(old_path)
 
-    assert not old_path.exists()
+    assert old_path.exists()
 
     m = type("Missing", (), {"__repr__": lambda _self: "<missing>"})()
     keys = ("s1", "k1", m), ("s2", "k2", m), ("s2", "k3", m), ("s2", "k4", m), ("s2", "k5", m)
     assert preferences.getmany(*keys) == ("spam", 42, True, 0.42, None)
 
 
-def test_migrate_noop(tmp_path, preferences):
+def test_migrate_invalid_data(tmp_path, preferences):
     old_path = tmp_path / "old_preferences.pickle"
-    old_path.touch()
+    old_path.write_bytes(pickle.dumps(object()))
 
     preferences.migrate(old_path)
 
