a67c466a495910d5b2d8219f8de152e8c64e1aee
k-dominik <k-dominik@users.noreply.github.com>
2022-09-21 12:49:37 -0700

    make sure to skip 3d widget test if disabled by environment env

--- tests/glview_test.py
+++ tests/glview_test.py
@@ -1,9 +1,13 @@
 from unittest.mock import patch, PropertyMock
 import importlib
+import os
+
+import pytest
 
 import volumina.view3d.glview as glview
 
 
+@pytest.mark.skipif(os.environ.get("VOLUMINA_SHOW_3D_WIDGET", "1") == "0", reason="widget disabled in test env")
 def test_import_gl_enabled():
     with patch("volumina.config._Config.show_3d_widget", new=PropertyMock(return_value=True)):
         importlib.reload(glview)
