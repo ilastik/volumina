e2d879c8a3fa5efc84602214a9ce5eaa990b8049
k-dominik <k-dominik@users.noreply.github.com>
2024-08-21 11:58:44 +0200

    Add more valueRangeWidget tests

--- tests/valueRangeWidget_test.py
+++ tests/valueRangeWidget_test.py
@@ -3,13 +3,17 @@ import pytest
 from volumina.widgets.valueRangeWidget import ValueRangeWidget
 
 
-@pytest.mark.parametrize("dtype", [numpy.float32, numpy.float64, float])
-def test_floating_range(qtbot, dtype):
-    """repro of https://github.com/ilastik/ilastik/issues/2542"""
+@pytest.fixture
+def vrwidget(qtbot):
     vrwidget = ValueRangeWidget()
     qtbot.addWidget(vrwidget)
     vrwidget.show()
+    return vrwidget
 
+
+@pytest.mark.parametrize("dtype", [numpy.float32, numpy.float64, float])
+def test_floating_range(vrwidget, qtbot, dtype):
+    """repro of https://github.com/ilastik/ilastik/issues/2542"""
     with qtbot.waitExposed(vrwidget):
         vrwidget.setDType(dtype)
 
@@ -19,14 +23,49 @@ def test_floating_range(qtbot, dtype):
 
 
 @pytest.mark.parametrize("dtype", [numpy.int8, numpy.uint8, numpy.int16, numpy.uint16, numpy.int32, numpy.uint32, int])
-def test_integer_range(qtbot, dtype):
-    vrwidget = ValueRangeWidget()
-    qtbot.addWidget(vrwidget)
-    vrwidget.show()
-
+def test_integer_range(vrwidget, qtbot, dtype):
     with qtbot.waitExposed(vrwidget):
         vrwidget.setDType(dtype)
 
     minmax = vrwidget.getValues()
     minmax_expected = numpy.iinfo(dtype).min, numpy.iinfo(dtype).max
     numpy.testing.assert_array_equal(minmax, minmax_expected)
+
+
+def test_setBlank(vrwidget, qtbot):
+    with qtbot.waitExposed(vrwidget):
+        vrwidget.setBlank()
+
+    assert vrwidget._blank == True
+    assert vrwidget.minBox.specialValueText() == "--"
+    assert vrwidget.maxBox.specialValueText() == "--"
+
+
+def test_onChangedMinBox(vrwidget, qtbot):
+    """Test behavior of max box being incremented if user adds the same value into min"""
+    with qtbot.waitExposed(vrwidget):
+        vrwidget.setDType(int)
+        vrwidget.setLimits(0, 10)
+        vrwidget.maxBox.setValue(5)
+        vrwidget.minBox.setValue(5)
+    assert int(vrwidget.minBox.value()) == 5
+    assert int(vrwidget.maxBox.value()) == 6
+
+
+def test_onChangedMaxBox(vrwidget, qtbot):
+    """Test behavior of min box being decremented if user adds the same value into max"""
+    with qtbot.waitExposed(vrwidget):
+        vrwidget.setDType(int)
+        vrwidget.setLimits(0, 10)
+        vrwidget.minBox.setValue(5)
+        vrwidget.maxBox.setValue(5)
+    assert int(vrwidget.maxBox.value()) == 5
+    assert int(vrwidget.minBox.value()) == 4
+
+
+def test_setValues(vrwidget, qtbot):
+    with qtbot.waitExposed(vrwidget):
+        vrwidget.setDType(int)
+        vrwidget.setValues(2, 8)
+    assert int(vrwidget.minBox.value()) == 2
+    assert int(vrwidget.maxBox.value()) == 8
