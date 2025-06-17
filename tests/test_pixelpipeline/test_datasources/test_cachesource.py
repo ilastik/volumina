a3f2c79c52244bed3572090a642d52ec09ee5e11
Benedikt Best <63287233+btbest@users.noreply.github.com>
2025-06-17 12:05:35 +0200

    Refactor test_cachesource

--- tests/test_pixelpipeline/test_datasources/test_cachesource.py
+++ tests/test_pixelpipeline/test_datasources/test_cachesource.py
@@ -33,54 +33,58 @@ class DummySource(QObject):
         return self._Req(self._data[slicing])
 
 
-class TestCacheSource:
-    @pytest.fixture
-    def orig_source(self):
-        arr = np.arange(60).reshape(3, 4, 5)
-        source = DummySource(arr)
-        return mock.Mock(wraps=source)
+@pytest.fixture
+def raw_source():
+    arr = np.arange(60).reshape(3, 4, 5)
+    source = DummySource(arr)
+    return mock.Mock(wraps=source)
 
-    @pytest.fixture
-    def cached_source(self, orig_source):
-        return CacheSource(orig_source)
 
-    def test_consecutive_requests_are_cached(self, cached_source, orig_source):
-        slicing = np.s_[1:2, 2:3, 3:4]
-        res0 = cached_source.request(slicing).wait()
-        res1 = cached_source.request(slicing).wait()
+@pytest.fixture
+def cached_source(raw_source):
+    return CacheSource(raw_source)
 
-        assert_array_equal(np.array([[[33]]]), res0)
-        assert_array_equal(np.array([[[33]]]), res1)
 
-        orig_source.request.assert_called_once_with(slicing)
+def test_consecutive_requests_are_cached(cached_source, raw_source):
+    slicing = np.s_[1:2, 2:3, 3:4]
+    res0 = cached_source.request(slicing).wait()
+    res1 = cached_source.request(slicing).wait()
 
-    def test_cache_invalidation(self, cached_source, orig_source):
-        slicing = np.s_[2:3, 0:1, 2:3]
-        res0 = cached_source.request(slicing).wait()
-        res1 = cached_source.request(slicing).wait()
+    assert_array_equal(np.array([[[33]]]), res0)
+    assert_array_equal(np.array([[[33]]]), res1)
 
-        assert_array_equal(np.array([[[42]]]), res0)
-        assert_array_equal(np.array([[[42]]]), res1)
+    raw_source.request.assert_called_once_with(slicing)
 
-        orig_source.set_data(np.arange(27, 87).reshape(3, 4, 5))
-        res2 = cached_source.request(slicing).wait()
-        assert_array_equal(np.array([[[69]]]), res2)
-        assert orig_source.request.call_count == 2
 
-    def test_cache_results_are_readonly(self, cached_source, orig_source):
-        slicing = np.s_[2:3, 0:1, 2:3]
-        res = cached_source.request(slicing).wait()
+def test_cache_invalidation(cached_source, raw_source):
+    slicing = np.s_[2:3, 0:1, 2:3]
+    res0 = cached_source.request(slicing).wait()
+    res1 = cached_source.request(slicing).wait()
 
-        with pytest.raises(ValueError):
-            res[0] = 100
+    assert_array_equal(np.array([[[42]]]), res0)
+    assert_array_equal(np.array([[[42]]]), res1)
 
-    def test_cache_if_value_is_too_large(self, orig_source):
-        cached_source = CacheSource(orig_source, cache=KVCache(1, getsizeof=sys.getsizeof))
-        slicing = np.s_[2:3, 0:1, 2:3]
-        res0 = cached_source.request(slicing).wait()
-        res1 = cached_source.request(slicing).wait()
+    raw_source.set_data(np.arange(27, 87).reshape(3, 4, 5))
+    res2 = cached_source.request(slicing).wait()
+    assert_array_equal(np.array([[[69]]]), res2)
+    assert raw_source.request.call_count == 2
 
-        assert_array_equal(np.array([[[42]]]), res0)
-        assert_array_equal(np.array([[[42]]]), res1)
 
-        assert orig_source.request.call_count == 2
+def test_cache_results_are_readonly(cached_source):
+    slicing = np.s_[2:3, 0:1, 2:3]
+    res = cached_source.request(slicing).wait()
+
+    with pytest.raises(ValueError):
+        res[0] = 100
+
+
+def test_cache_if_value_is_too_large(raw_source):
+    cached_source = CacheSource(raw_source, cache=KVCache(1, getsizeof=sys.getsizeof))
+    slicing = np.s_[2:3, 0:1, 2:3]
+    res0 = cached_source.request(slicing).wait()
+    res1 = cached_source.request(slicing).wait()
+
+    assert_array_equal(np.array([[[42]]]), res0)
+    assert_array_equal(np.array([[[42]]]), res1)
+
+    assert raw_source.request.call_count == 2
