5103cc875967c39b0e49f77d0d66b424104e4e3d
k-dominik <k-dominik@users.noreply.github.com>
2025-08-27 06:27:30 +0200

    Fix metaclass

--- volumina/utility/qabc.py
+++ volumina/utility/qabc.py
@@ -2,7 +2,7 @@
 Wraps stdlib abc module to provide convenience classes for defining QObject interfaces
 """
 
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, _abc_init
 
 from qtpy.QtCore import QObject
 
@@ -21,8 +21,24 @@ class abstractsignal:
         pass
 
 
-class QABCMeta(type(QObject), ABCMeta):
-    pass
+class QABCMeta(ABCMeta, type(QObject)):
+    """
+    from: https://stackoverflow.com/a/78794968
+
+    tested with PyQt5, PySide6
+    """
+
+    def __new__(mcls, name, bases, ns, **kw):
+        cls = super().__new__(mcls, name, bases, ns, **kw)
+        _abc_init(cls)
+        return cls
+
+    def __call__(cls, *args, **kw):
+        if cls.__abstractmethods__:
+            raise TypeError(
+                f"Can't instantiate abstract class {cls.__name__} without an implementation for abstract methods {set(cls.__abstractmethods__)}"
+            )
+        return super().__call__(*args, **kw)
 
 
 class QABC(metaclass=QABCMeta):
